0b2d524a898d8f37f7f38438cb55573f092e3f67
Corey Sheesley <corey.sheesley@gmail.com>
2019-12-31 14:51:39 -0700

    Initial component scaffolding

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// component determines correct display for season
+const SeasonDisplay = () => {
+  return(
+    <div>Season Diplay</div>
+  );
+};
+
+export default SeasonDisplay;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// component determines location and month
+const App = () => {
+  return(
+    <div>Hello there!</div>
+  );
+};
+
+ReactDOM.render(
+  <App />, document.querySelector('#root')
+)
