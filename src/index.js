fffb94100cca8aa6dfad8869196e3b8c5c9b72dd
Corey Sheesley <corey.sheesley@gmail.com>
2019-12-31 15:09:45 -0700

    Get geolocation from browser

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 // component determines location and month
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    // success callback
+    (position) => console.log(position),
+    (error) => console.log(error)
+    // error callback
+  );
+
   return(
     <div>Hello there!</div>
   );
