2b19472955ec2bcd8d81a6170bb359144671a4cf
Corey Sheesley <corey.sheesley@gmail.com>
2020-01-01 11:09:42 -0700

    Conditional rendering and handling of error - first rough approach

--- src/index.js
+++ src/index.js
@@ -6,23 +6,35 @@ class App extends React.Component {
   constructor (props) {
     super(props);
 
-    this.state = { lat: null, lng: null };
+    this.state = { lat: null, lng: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude, lng: position.coords.longitude })
       },
-      (error) => console.log(error)
+      (error) => {
+        this.setState({ errorMessage: error.message})
+      }
     );
   }
 
   render() {
-    return (
-      <div className="coordinates">
-        <div className="latitide">Latitude: { this.state.lat }</div>
-        <div className="longitude">Longitude: { this.state.lng }</div>
-      </div>
-    );
+    if (this.state.lat && this.state.lng && !this.state.errorMessage) {
+      return (
+        <div className="coordinates">
+          <div className="latitide">Latitude: { this.state.lat }</div>
+          <div className="longitude">Longitude: { this.state.lng }</div>
+        </div>
+      )
+    }
+
+    if (this.state.errorMessage) {
+      return (
+        <div className="error-message">Error: { this.state.errorMessage }</div>
+      )
+    }
+
+    return <div className="loading">Loading!</div>
   }
 }
 
